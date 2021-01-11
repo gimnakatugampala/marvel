4a6b229f787450dee04524fd593a3b020cc39d0c
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-01-12 01:22:11 +0530

    asynxc

--- js/marvel.api.js
+++ js/marvel.api.js
@@ -3,7 +3,7 @@ const publickey = '48342c5f372aa81b8cd704e870424c46';
 const hash  = 'd41d8cd98f00b204e9800998ecf8427e';
 
 //fetch all characters
-function getCharacters(){
+async function getCharacters(){
     const res = await fetch(`https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${publickey}&hash=${hash}`);
     const chraracters = res.json();
 
