f3f882001f6f48ed4410abc9a4ba00f5ed6bbad7
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-01-12 01:31:03 +0530

    comics para

--- js/marvel.api.js
+++ js/marvel.api.js
@@ -4,7 +4,7 @@ const hash  = 'a2f024a0b57719f14d611e3c9c4e71df';
 
 //fetch all characters
 async function getCharacters(){
-    const res = await fetch(`https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${publickey}&hash=${hash}`);
+    const res = await fetch(`https://gateway.marvel.com/v1/public/comics?ts=1&apikey=${publickey}&hash=${hash}`);
     const chraracters = res.json();
 
     return chraracters;
