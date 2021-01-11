9b6a058074db23ca5bfd0bee021d914777378cc6
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-01-12 02:03:44 +0530

    hilk

--- js/main.js
+++ js/main.js
@@ -1,7 +1,2 @@
 
 getCharacters().then(data => console.log(data))
-getComics().then(data => console.log(data))
-getCreators().then(data => console.log(data))
-getEvents().then(data => console.log(data))
-getSeries().then(data => console.log(data))
-getStories().then(data => console.log(data))
