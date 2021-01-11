8d1b7cc021e17ee3223ebef550d2ad101cc8a046
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-01-12 03:50:32 +0530

    materilize

--- js/main.js
+++ js/main.js
@@ -6,3 +6,11 @@ const searchName = document.getElementById('name');
 getCharacters()
 .then(data => console.log(data))
 
+
+
+
+    //Slider
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, {
+        indicators: false
+    });
