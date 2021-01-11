42d59ae0505889def01b5d115bb29c3ef6768a2e
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-01-12 01:53:26 +0530

    fucntions

--- js/main.js
+++ js/main.js
@@ -1,2 +1,7 @@
 
 getCharacters().then(data => console.log(data))
+getComics().then(data => console.log(data))
+getCreators().then(data => console.log(data))
+getEvents().then(data => console.log(data))
+getSeries().then(data => console.log(data))
+getStories().then(data => console.log(data))
--- js/marvel.api.js
+++ js/marvel.api.js
@@ -4,8 +4,50 @@ const hash  = 'a2f024a0b57719f14d611e3c9c4e71df';
 
 //fetch all characters
 async function getCharacters(){
-    const res = await fetch(`https://gateway.marvel.com/v1/public/events?ts=1&apikey=${publickey}&hash=${hash}`);
+    const res = await fetch(`https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${publickey}&hash=${hash}`);
     const chraracters = res.json();
 
     return chraracters;
+}
+
+
+//fetch all Comics
+async function getComics(){
+    const res = await fetch(`https://gateway.marvel.com/v1/public/comics?ts=1&apikey=${publickey}&hash=${hash}`);
+    const comics = res.json();
+
+    return comics;
+}
+
+//fetch all Creators
+async function getCreators(){
+    const res = await fetch(`https://gateway.marvel.com/v1/public/creators?ts=1&apikey=${publickey}&hash=${hash}`);
+    const creators = res.json();
+
+    return creators;
+}
+
+//fetch all Events
+async function getEvents(){
+    const res = await fetch(`https://gateway.marvel.com/v1/public/events?ts=1&apikey=${publickey}&hash=${hash}`);
+    const events = res.json();
+
+    return events;
+}
+
+//fetch all Series
+async function getSeries(){
+    const res = await fetch(`https://gateway.marvel.com/v1/public/series?ts=1&apikey=${publickey}&hash=${hash}`);
+    const series = res.json();
+
+    return series;
+}
+
+
+//fetch all Stories
+async function getStories(){
+    const res = await fetch(`https://gateway.marvel.com/v1/public/stories?ts=1&apikey=${publickey}&hash=${hash}`);
+    const stories = res.json();
+
+    return stories;
 }
