6aaec915351f6206d1b73443069c6911be61b00a
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-01-12 05:47:00 +0530

    fetch

--- js/main.js
+++ js/main.js
@@ -1,10 +1,77 @@
-const searchHero = document.getElementById('enterHero');
+const searchHero = document.getElementById('enterHeros');
 const searchName = document.getElementById('name');
+const appendInfo = document.getElementById('info-append');
+
 
 //submit the search hero Term
+searchHero.addEventListener('submit',submitName);
 
-getCharacters()
-.then(data => console.log(data))
+//Submit the searh heros
+function submitName(e){
+    e.preventDefault();
+    getCharacters(searchName.value)
+    .then(data => {
+        console.log(data.data.results[0])
+      const info =  data.data.results[0];
+        appendInfo.innerHTML = `
+        <div class="row">
+          <div class="col s12 m5">
+            <div class="card">
+              <div class="card-image">
+                <img src="${info.thumbnail.path}.${info.thumbnail.extension}" />
+                <span class="card-title">${info.name}</span>
+              </div>
+            </div>
+          </div>
+          <div class="col s12 m7">
+            <h5>${info.name}'s Info</h5>
+            <p>
+             ${info.description}
+            </p>
+            <button class="btn grey darken-4 get-btn" onclick="clickComics(${info.id})">
+              get Comics
+            </button>
+            <button class="btn grey darken-3 get-btn" onclick="clickEvents(${info.id})">
+              get Events
+            </button>
+            <button class="btn grey darken-2 get-btn" onclick="clickCreators(${info.id})">
+              get Creators
+            </button>
+            <button class="btn grey darken-1 get-btn" onclick="clickStories(${info.id})">
+              get Stories
+            </button>
+          </div>
+        </div>
+        
+        `;
+    })
+    .catch(err => console.log(err))
+
+}
+
+    //click to get Comics
+    function clickComics(id){
+        getComics(id)
+        .then(data => console.log(data))
+    }
+
+    //click to get events
+    function clickEvents(id){
+        console.log(id)
+    }
+
+    //clik to get Creators
+    function clickCreators(id){
+        console.log(id)
+    }
+
+
+    //click to get stories
+    function clickStories(id){
+        console.log(id)
+    }
+
+
 
 
 
--- js/marvel.api.js
+++ js/marvel.api.js
@@ -3,9 +3,17 @@ const publickey = '48342c5f372aa81b8cd704e870424c46';
 const hash  = 'a2f024a0b57719f14d611e3c9c4e71df';
 
 //fetch all characters
-async function getCharacters(){
-    const res = await fetch(`https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${publickey}&hash=${hash}&name=hulk`);
+async function getCharacters(name){
+    const res = await fetch(`https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${publickey}&hash=${hash}&name=${name}`);
     const chraracters = res.json();
 
     return chraracters;
+}
+
+//Fetch all omis of the id
+async function getComics(id){
+    const res = await fetch(`https://gateway.marvel.com/v1/public/characters/${id}/comic?ts=1&apikey=${publickey}&hash=${hash}`);
+    const comics = res.json();
+
+    return comics;
 }
